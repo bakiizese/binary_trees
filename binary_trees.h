16fcf69b4ad55977f0bc0b74badf2c5f345eb452
bakiizese <bereketzeselassie@gmail.com>
2023-11-28 22:14:59 -0800

    gfix

--- binary_trees.h
+++ binary_trees.h
@@ -17,9 +17,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+} binary_tree_t ;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 
 
