5ec70f9ac57754d5d36f50c4ddb088bdca2cbe2c
bakiizese <bereketzeselassie@gmail.com>
2023-11-29 00:19:24 -0800

    fix

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+/**
+ * depth - entry
+ * @tree: pointer
+ * Return: num of deopth
+ */
+int depth(const binary_tree_t *tree)
+{
+	int pra;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	pra = depth(tree->parent);
+
+	return (pra + 1);
+}
+/**
+ * binary_trees_ancestor - entry
+ * @first: pointer
+ * @second: ponter
+ * Return: LCA
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	int dpt;
+	int dps;
+
+	if ((first->n == second->n) || (first->parent == NULL &&
+				second->parent == NULL))
+	{
+		binary_tree_t *new = (binary_tree_t *) first;
+
+		return (new);
+	}
+	dpt = depth(first);
+	dps = depth(second);
+
+	if (dpt < dps)
+		return (binary_trees_ancestor(first, second->parent));
+	else if (dpt > dps)
+		return (binary_trees_ancestor(first->parent, second));
+	else if (dpt == dps)
+		return (binary_trees_ancestor(first->parent, second->parent));
+
+	return (0);
+}
